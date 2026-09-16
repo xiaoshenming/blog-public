be16773d0f8930a333ea141b7d2665806aeeb7f9
xiaoshenming <1181584752@qq.com>
2026-09-16 16:51:14 +0800

    fix: 移除 fork 自带的上游 Google Analytics ID

--- src/layout/head.tsx
+++ src/layout/head.tsx
@@ -1,5 +1,3 @@
-import Script from 'next/script'
-
 export default function Head() {
 	return (
 		<head>
@@ -7,17 +5,6 @@ export default function Head() {
 			<link rel='manifest' href='/manifest.json' />
 
 			<link rel='icon' href='/favicon.png' />
-
-			<Script src='https://www.googletagmanager.com/gtag/js?id=G-ZNSFR7C9PM' />
-			<Script id='google-analytics'>
-				{`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-ZNSFR7C9PM');
-        `}
-			</Script>
 		</head>
 	)
 }
